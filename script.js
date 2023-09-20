8e45a1955b6db05c9a8cedc0e807c5d0027a5f6c
WilfredMutwiri <wilfredmutwiri20@gmail.com>
2023-09-20 21:18:21 +0300

    add navbar media queries

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-// login page
-// forgot password
-let passRest=document.querySelector("#passReset");
-passRest.addEventListener("click",resetPop);
-function resetPop(){
-    let email=prompt(`Enter the email address used to register your account.`);
-    if(email.endsWith("@gmail.com")===false){
-        alert("Enter correct email address")
-    }
-    else{
-        alert(`An email has been sent to ${email} successfully, Kindly log into the email address for more instructions.`);
-    }
+
+// reveal navbar
+let navReveal = document.querySelector('#menu svg')
+let navLinks = document.querySelector('#navHolder ul')
+navReveal.addEventListener('click',(e)=> {
+  navLinks.classList.toggle('active')
+});
+// password reset
+let passRest = document.querySelector('#passReset')
+passRest.addEventListener('click', resetPop)
+ function resetPop(){
+  let email = prompt(`Enter the email address used to register your account.`)
+  if (email.endsWith('@gmail.com') === false) {
+    alert('Enter correct email address')
+  } else {
+    alert(
+      `An email has been sent to ${email} successfully, Kindly log into the email address for more instructions.`
+    )
+  }
 }
+
+
