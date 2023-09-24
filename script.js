7c0bf790512daf9df86bbd1da7937c1feda7461f
WilfredMutwiri <wilfredmutwiri20@gmail.com>
2023-09-24 13:08:52 +0300

    add about section

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// // password reset
+// let passRest = document.querySelector('#passReset')
+// passRest.addEventListener('click', resetPop)
+//  function resetPop(){
+//   let email = prompt(`Enter the email address used to register your account.`)
+//   if (email.endsWith('@gmail.com') === false) {
+//     alert('Enter correct email address')
+//   } else {
+//     alert(
+//       `An email has been sent to ${email} successfully, Kindly log into the email address for more instructions.`
+//     )
+//   }
+// }
 
 // reveal navbar
 let navReveal = document.querySelector('#menu svg')
@@ -5,18 +18,10 @@ let navLinks = document.querySelector('#navHolder ul')
 navReveal.addEventListener('click',(e)=> {
   navLinks.classList.toggle('active')
 });
-// password reset
-let passRest = document.querySelector('#passReset')
-passRest.addEventListener('click', resetPop)
- function resetPop(){
-  let email = prompt(`Enter the email address used to register your account.`)
-  if (email.endsWith('@gmail.com') === false) {
-    alert('Enter correct email address')
-  } else {
-    alert(
-      `An email has been sent to ${email} successfully, Kindly log into the email address for more instructions.`
-    )
-  }
-}
-
+// faqs reveal
+// let quiz=document.getElementById("faqs");
+// let answer=document.getElementById("ans")
+// quiz.addEventListener("click",(e)=>{
+//   answer.style.left="auto";
+// })
 
